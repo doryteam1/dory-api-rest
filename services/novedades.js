2df24a63c0d1e2083ca150ba245da0b6a490a73a
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2021-11-30 12:23:16 -0500

    Obtención de novedades corregida

--- services/novedades.js
+++ services/novedades.js
@@ -5,13 +5,15 @@ const config = require('../config');
 async function getMultiple(page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT distinctrow  n.id_novedad,  n.autor,n.url_foto_autor,n.url_foto_novedad,n.titulo,n.resumen,n.fecha_creacion,n.cant_visitas,
+    `SELECT distinctrow  n.id_novedad,  n.autor,n.url_foto_autor,n.url_foto_novedad,n.titulo,n.resumen,n.fecha_creacion,n.cant_visitas ,
     (select count(*) from me_gustas m1,novedades n1 where m1.id_novedad_pk_fk=n1.id_novedad and m1.id_novedad_pk_fk= n.id_novedad) as likes ,
      n.url_novedad,n.email_autor,n.canal,n.cuerpo,n.id_tipo_novedad as tipo_novedad,tn.nombre as tipo,
-     c.id_categoria,
-     c.nombre_categoria,c.descripcion_categoria
-FROM novedades n,me_gustas m,categorias c,categorias_novedades cn,tipos_novedades tn
-where (n.id_tipo_novedad=tn.id_tipo_novedad) and (c.id_categoria=cn.id_categoria_pk_fk) and (cn.id_novedad_pk_fk=n.id_novedad)
+     c.id_categoria,  c.nombre_categoria,c.descripcion_categoria
+     
+FROM  novedades as n left join me_gustas m on (m.id_novedad_pk_fk=n.id_novedad)
+                     left join tipos_novedades as tn on (n.id_tipo_novedad=tn.id_tipo_novedad)
+                     left join categorias_novedades as cn on (cn.id_novedad_pk_fk=n.id_novedad)
+                     left join categorias as c on (cn.id_categoria_pk_fk=c.id_categoria)
 order by  n.id_novedad
 LIMIT ?,?`, 
     [offset, config.listPerPage]
