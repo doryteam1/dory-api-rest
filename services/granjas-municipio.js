227389d5b861045c9d7ded7ad07efe13ee3a7396
Renzo Funes Meza <rfunez@gmail.com>
2021-12-02 07:41:39 -0500

    Revert "granjas-municipio ajuste"
    This reverts commit 1fd949963e5ce3b5c54d6467029c3d673c605e3b.

--- services/granjas-municipio.js
+++ services/granjas-municipio.js
@@ -5,14 +5,13 @@ const config = require('../config');
 async function getMultiple(page = 1,id){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `select distinct     g.id_granja, g.nombre,g.area, g.numero_trabajadores, g.produccion_estimada_mes, g.direccion,g.descripcion,f.id_foto,f.imagen,(select count(*) from reseñas r1,granjas g1 where r1.id_granja_pk_fk=g1.id_granja and r1.id_granja_pk_fk= g.id_granja) as count_reseñas,
-    (select avg(puntuacion) from usuarios_granjas ug5 where g.id_granja=ug5.id_granja_pk_fk ) as puntuacion
-from fotos as f, granjas as g, usuarios_granjas ug
-where (f.id_granja_fk = g.id_granja) and
-g.id_granja  in( select ug2.id_granja_pk_fk 
-           from usuarios_granjas AS ug2,  granjas AS g2
-          where g2.id_granja=ug2.id_granja_pk_fk and ug.id_granja_pk_fk=ug2.id_granja_pk_fk) and
-     g.id_municipio=?
+    `SELECT distinctrow g.id_granja,g.nombre,g.area,g.numero_trabajadores,g.produccion_estimada_mes,g.direccion,g.latitud,g.longitud,g.descripcion,g.id_departamento,g.id_municipio,g.id_corregimiento,g.id_vereda,f.imagen,ug.puntuacion,ug.espropietario,ug.esfavorita,
+                       (select count(*) from reseñas r1,granjas g1 where r1.id_granja_pk_fk=g1.id_granja and r1.id_granja_pk_fk= g.id_granja) as count_reseñas
+     FROM  granjas as g left join fotos f on (g.id_granja=f.id_foto)
+                      left join reseñas as r on (g.id_granja=r.id_granja_pk_fk)
+                      left join usuarios_granjas as ug on (g.id_granja=ug.id_granja_pk_fk)
+                      left join usuarios as u on (ug.cedula_usuario_pk_fk=u.cedula)
+     WHERE  g.id_municipio=?
            LIMIT ?,?`, 
     [id,offset, config.listPerPage]
   );
