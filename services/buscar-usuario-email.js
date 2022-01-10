8eefdf6ab575732ae1d24af69683cc0ad7a5eb6d
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-01-10 15:26:55 -0500

    busqueda usuario por email completado

--- services/buscar-usuario-email.js
+++ services/buscar-usuario-email.js
@@ -7,11 +7,7 @@ async function getMultiple(page = 1, email){
   const rows = await db.query(
     `SELECT distinctrow   u.cedula,concat(u.nombres," ",u.apellidos) as nombre_completo,
     u.celular,u.direccion,u.email,u.password,tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.id_area_experticia,
-    (select a.nombre from areas_experticias a  where a.id_area=u.id_area_experticia) as area_experticia,u.nombre_negocio,u.foto,u.fecha_registro,u.fecha_nacimiento,
-    (select d.nombre_departamento from departamentos d  where d.id_departamento=u.id_departamento) as departamento,
-    (select m.nombre from municipios as m  where m.id_municipio=u.id_municipio) as municipio,
-    (select c.nombre from corregimientos as c  where c.id_corregimiento=u.id_corregimiento) as corregimiento,
-    (select v.nombre from veredas as v  where v.id_vereda=u.id_vereda) as vereda,
+    
     u.latitud,u.longitud
     FROM tipos_usuarios as tu inner join usuarios as u on u.id_tipo_usuario=tu.id_tipo_usuario
     WHERE   u.email=?
