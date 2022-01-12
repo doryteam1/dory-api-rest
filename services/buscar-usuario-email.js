e435af7b86ec0bfd5dd0211e0bf981faab056ee8
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-01-12 17:54:26 -0500

    busqueda de usuarios modificado por nombres

--- services/buscar-usuario-email.js
+++ services/buscar-usuario-email.js
@@ -5,7 +5,7 @@ const config = require('../config');
 async function getMultiple(page = 1, email){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT  u.cedula,concat(u.nombres," ",u.apellidos) as nombre_completo,
+    `SELECT  u.cedula, u.nombres, u.apellidos 
     u.celular,u.direccion,u.email,u.password,tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.id_area_experticia,
     (select a.nombre from areas_experticias a  where a.id_area=u.id_area_experticia) as area_experticia,u.nombre_negocio,u.foto,u.fecha_registro,u.fecha_nacimiento,
     (select d.nombre_departamento from departamentos d  where d.id_departamento=u.id_departamento) as departamento,
