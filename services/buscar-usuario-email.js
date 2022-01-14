90db63551be53b24c24389feae06beb7bc6c398b
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-01-14 07:29:40 -0500

    correccion de busqueda de usuarios por email- actualizacion de id

--- services/buscar-usuario-email.js
+++ services/buscar-usuario-email.js
@@ -5,12 +5,16 @@ const config = require('../config');
 async function getMultiple(page = 1, email){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT  u.cedula, u.nombres, u.apellidos,
+    `SELECT  u.id,u.cedula, u.nombres, u.apellidos,
     u.celular,u.direccion,u.email,u.password,tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.id_area_experticia,
     (select a.nombre from areas_experticias a  where a.id_area=u.id_area_experticia) as area_experticia,u.nombre_negocio,u.foto,u.fecha_registro,u.fecha_nacimiento,
+    u.id_departamento,
     (select d.nombre_departamento from departamentos d  where d.id_departamento=u.id_departamento) as departamento,
+    u.id_municipio,
     (select m.nombre from municipios as m  where m.id_municipio=u.id_municipio) as municipio,
+    u.id_corregimiento,
     (select c.nombre from corregimientos as c  where c.id_corregimiento=u.id_corregimiento) as corregimiento,
+    u.id_vereda,
     (select v.nombre from veredas as v  where v.id_vereda=u.id_vereda) as vereda,
     u.latitud,u.longitud
     FROM usuarios as u left join tipos_usuarios as tu  on u.id_tipo_usuario=tu.id_tipo_usuario
