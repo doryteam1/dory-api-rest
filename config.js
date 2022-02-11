c3982b6e9cc5a2f7de8f0a6fb7c406eebb07cab9
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-02-10 19:24:17 -0500

    Corección del error de conexion con pool

--- config.js
+++ config.js
@@ -2,11 +2,14 @@ const env = process.env;
 
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || '',
-    database: env.DB_NAME || 'piscicola',
-  },
+  host: env.DB_HOST || 'localhost',
+  user: env.DB_USER || 'root',
+  password: env.DB_PASSWORD || '',
+  database: env.DB_NAME || 'piscicola',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+},
   TOKEN_SECRET: env.TOKEN_SECRET || "tokenultrasecreto",
   listPerPage: env.LIST_PER_PAGE || 1000,
 };
