2d0d1017e016bea6accbe4d17da93c88fbb06af5
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-02-25 18:08:23 -0500

    endpoint login asegurado

--- services/login.js
+++ services/login.js
@@ -1,37 +1,45 @@
 const db = require('./db');
 const helper = require('../helper');
-const config = require('../config');
 const bcrypt= require('bcrypt');
-const jwt = require('jwt-simple');
-var moment = require('moment');
-
+var createError = require('http-errors');
 
 async function createLogin(user){
 
   let email=user.email;
-  
-   const rows = await db.query(
-    `SELECT u.password,u.email,u.id,tu.nombre_tipo_usuario
-     FROM usuarios as u, tipos_usuarios as tu
-     WHERE u.id_tipo_usuario=tu.id_tipo_usuario and
-           u.email=? 
-    `, 
-    [email]
-  );
-  console.log(user);
   let message = "El usuario no esta registrado o la contraseña es inválida";
   
-  if(rows!=null && rows.length>0){
- 
-    let pass=rows[0].password;
-    let passUser=user.password;
+  if(email!=undefined && email!=null)
+  {
 
-    if(( bcrypt.compareSync(passUser,pass))){
-          var token=helper.createToken(rows[0]);
-         return {token:token};
-    }
+      try {
+            const rows = await db.query(
+              `SELECT u.password,u.email,u.id,tu.nombre_tipo_usuario
+              FROM usuarios as u, tipos_usuarios as tu
+              WHERE u.id_tipo_usuario=tu.id_tipo_usuario and
+                    u.email=? 
+              `, 
+              [email]
+            );
+                              
+            if(rows!=null && rows.length>0){
+          
+              let pass=rows[0].password;
+              let passUser=user.password;
+
+              if(( bcrypt.compareSync(passUser,pass))){
+                    var token=helper.createToken(rows[0]);
+                  return {token:token};
+              }
+            }
+              throw createError(404,"Usuario no existe");
+
+      }catch{
+              throw createError(400,message);
+      }
+     
   }
-     return{message};
+  throw createError(400,message);
+     
 }//fin método
 
 
