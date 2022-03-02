f3dbad363d75fde9f02f0bea13687f212dae9678
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-03-02 18:13:32 -0500

    modificacion de novedades para usuarios no autenticados

--- services/novedades.js
+++ services/novedades.js
@@ -6,9 +6,12 @@ var createError = require('http-errors');
 async function getMultiple(page = 1,token){
 
       const payload=helper.parseJwt(token);
-      
       const offset = helper.getOffset(page, config.listPerPage);
-      const rows = await db.query(
+      const rows=[];
+
+      if(token){
+        
+         rows = await db.query(
         `SELECT distinctrow  n.id_novedad,  n.autor,n.url_foto_autor,n.url_foto_novedad,n.titulo,n.resumen,n.fecha_creacion,n.cant_visitas ,
                 (select count(*) from me_gustas m1,novedades n1 where m1.id_novedad_pk_fk=n1.id_novedad and m1.id_novedad_pk_fk= n.id_novedad) as likes ,
                 n.url_novedad,n.email_autor,n.canal,n.cuerpo,n.id_tipo_novedad as tipo_novedad,tn.nombre as tipo,
@@ -22,6 +25,23 @@ async function getMultiple(page = 1,token){
           LIMIT ?,?`, 
         [payload.sub,payload.sub,offset, config.listPerPage]
       );
+    }else{
+           rows = await db.query(
+      `SELECT distinctrow  n.id_novedad,  n.autor,n.url_foto_autor,n.url_foto_novedad,n.titulo,n.resumen,n.fecha_creacion,n.cant_visitas ,
+              (select count(*) from me_gustas m1,novedades n1 where m1.id_novedad_pk_fk=n1.id_novedad and m1.id_novedad_pk_fk= n.id_novedad) as likes ,
+              n.url_novedad,n.email_autor,n.canal,n.cuerpo,n.id_tipo_novedad as tipo_novedad,tn.nombre as tipo,
+              c.id_categoria,  c.nombre_categoria,c.descripcion_categoria,
+              (select count(*) from me_gustas as m2,novedades as n2, usuarios as u where m2.id_novedad_pk_fk=n2.id_novedad and m2.usuarios_id=u.id and m2.id_novedad_pk_fk=n2.id_novedad and n2.id_novedad=n.id_novedad) as me_gusta          
+              FROM  novedades as n left join me_gustas m on (m.id_novedad_pk_fk=n.id_novedad)
+                            left join tipos_novedades as tn on (n.id_tipo_novedad=tn.id_tipo_novedad)
+                            left join categorias_novedades as cn on (cn.id_novedad_pk_fk=n.id_novedad)
+                            left join categorias as c on (cn.id_categoria_pk_fk=c.id_categoria)
+        order by  n.id_novedad
+        LIMIT ?,?`, 
+      [offset, config.listPerPage]
+    );
+
+    }
 
         var arraycategorias= new Array();
         var nuevoRows = new Array();
