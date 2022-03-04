43f896055c4547c29bd8f7bb14315c4f906d0545
Renzo Funes Meza <rfunez@gmail.com>
2022-03-03 23:21:27 -0500

    cors

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const loginRouter = require('./routes/login');
 const contactenosRouter = require('./routes/contactenos');
 const compartirRouter = require('./routes/compartir');
 const novedadesLikesRouter = require('./routes/novedades-likes');
-
+var cors = require('cors')
 
 app.use(bodyParser.json());
 app.use(
@@ -82,13 +82,15 @@ app.use(
 );
 
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-});
+});*/
+
+app.use(cors());
 
 app.route('/')
   .get(function (req, res) {
