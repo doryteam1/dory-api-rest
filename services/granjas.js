5acdf665bd3fa907496075da2ea5e1a1cc7b7a5e
Renzo Funes Meza <rfunez@gmail.com>
2022-04-25 17:43:14 -0500

    test

--- services/granjas.js
+++ services/granjas.js
@@ -135,7 +135,7 @@ async function create(body,token){
                 {
                   throw createError(400,"Se requieren todos los parámetros!");
                 }                 
-                console.log("inicio query 1")
+                console.log("inicio query 1",body)
                  const result = await db.query(
                     `INSERT INTO granjas (nombre,area,numero_trabajadores, produccion_estimada_mes,direccion,latitud,longitud,descripcion,id_departamento,id_municipio,id_corregimiento,id_vereda,anulado) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`, 
                     [
