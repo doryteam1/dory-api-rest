aba4ddc059c7e62ce1aa49ff601357d5979d305e
Renzo Funes Meza <rfunez@gmail.com>
2022-05-10 21:35:13 -0500

    test

--- index.js
+++ index.js
@@ -166,6 +166,7 @@ app.use((err, req, res, next) => {
     }
 
     const statusCode = err.statusCode || 500;
+    console.error(err)
     console.error(err.message, err.stack);
     res.status(statusCode).json({'message': err.message});
     return;
