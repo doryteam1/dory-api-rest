950ad75ef2098a506743fa7baa83063a2ea40f2b
Renzo Funes Meza <rfunez@gmail.com>
2022-05-10 21:45:32 -0500

    ajuste mensaje de error

--- index.js
+++ index.js
@@ -138,38 +138,42 @@ app.use('/api/novedades/auth',verifyToken,novedadesLikesRouter)
 /* Error handler middleware */
 app.use((err, req, res, next) => {     
   
-  
   if(err.code=='ER_DUP_ENTRY'){/* Clave primaria duplicada al enviar la solicitud */
     err.message="El registro ya existe";
     err.statusCode=400;
- }
+  }
  
   if(err.code=='ER_PARSE_ERROR'){
-      err.statusCode=400;
+    err.statusCode=400;
   }
            
  if(err.code=='ER_BAD_FIELD_ERROR'){/* Parametros mal escritos al enviar la solicitud */
-              err.statusCode=400;
-              err.message="No existe parametro en la BD"+" "+err.sqlMessage; 
+    err.statusCode=400;
+    err.message="No existe parametro en la BD"+" "+err.sqlMessage; 
   }
              
   if(err.code=='ER_WRONG_ARGUMENTS'){
-        err.statusCode=500;
+    err.statusCode=500;
   }
                 
   if(err.code=='ERR_INVALID_ARG_TYPE'){
-        err.statusCode=500;
+    err.statusCode=500;
   }
                  
   if(err.code=='EADDRINUSE'){
-         err.statusCode=500;
-    }
+    err.statusCode=500;
+  }
 
-    const statusCode = err.statusCode || 500;
-    console.error(err)
-    console.error(err.message, err.stack);
-    res.status(statusCode).json({'message': err.message});
-    return;
+  if(err.code == 'ER_NO_REFERENCED_ROW_2'){
+    err.statusCode = 400;
+    err.message = "El recurso al que intenta adicionar el registro no existe" 
+  }
+
+  const statusCode = err.statusCode || 500;
+  console.error(err)
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  return;
   });
 
 app.listen(port, () => {
