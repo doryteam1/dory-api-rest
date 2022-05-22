71633603d068270910b0a3a315ae9254eb9eb46e
Renzo Funes Meza <rfunez@gmail.com>
2022-05-22 09:39:19 -0500

    test

--- routes/granjas.js
+++ routes/granjas.js
@@ -139,7 +139,7 @@ router.put('/parcial/:id', async function(req, res, next) {
     try { 
          var token=req.headers.authorization;
          console.log("Token ",token)
-         console.log("req ",req)
+         console.log("req headers",req.headers)
          res.json(await granjas.esFavorita(req.params.idGranja,token));
     } catch (err) {
          console.error(`Error al agregar granja a favoritas`, err.message);
