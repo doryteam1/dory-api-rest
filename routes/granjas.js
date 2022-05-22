8e762081925ec447148d4becddaea64ab5c8cc46
Renzo Funes Meza <rfunez@gmail.com>
2022-05-22 09:06:55 -0500

    test

--- routes/granjas.js
+++ routes/granjas.js
@@ -147,6 +147,7 @@ router.put('/parcial/:id', async function(req, res, next) {
   router.put('/calificar/:idGranja', async function(req, res, next) {
     try { 
          var token=req.headers.authorization;
+         console.log("Token ",token)
          res.json(await granjas.esFavorita(req.params.idGranja,token,req.params.query));
     } catch (err) {
          console.error(`Error al calificar la granja`, err.message);
