806daa342499d540a9fc2474d8e595b28ce38b3c
Renzo Funes Meza <rfunez@gmail.com>
2022-05-27 07:30:58 -0500

    se agrego foto de usuario en get reseñas

--- services/resenas.js
+++ services/resenas.js
@@ -13,7 +13,8 @@ async function getResenasGranja(page = 1,idGranja){
     r.usuarios_id as id_usuario, 
     r.id_granja_pk_fk as id_granja, 
     g.nombre as nombre_granja,
-    (select concat(u.nombres,' ',u.apellidos) from usuarios as u inner join reseñas r2 on u.id = r.usuarios_id where r2.id_reseña = r.id_reseña) as nombre_usuario
+    (select concat(u.nombres,' ',u.apellidos) from usuarios as u inner join reseñas r2 on u.id = r.usuarios_id where r2.id_reseña = r.id_reseña) as nombre_usuario,
+    (select u.foto from usuarios as u inner join reseñas r2 on u.id = r.usuarios_id where r2.id_reseña = r.id_reseña) as foto_usuario
     FROM reseñas as r, granjas as g, usuarios_granjas as ug
     WHERE r.id_granja_pk_fk=g.id_granja and 
           g.id_granja=ug.id_granja_pk_fk and
