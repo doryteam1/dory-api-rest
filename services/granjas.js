c5444b98211af31132920aa1b405fe0d40b8bc27
Renzo Funes Meza <rfunez@gmail.com>
2022-06-05 10:15:25 -0500

    correccion puntuacion en mis granjas favoritas

--- services/granjas.js
+++ services/granjas.js
@@ -851,8 +851,9 @@ async function misFavoritas(token){
                 const rows = await db.query(
                   `SELECT  g.id_granja, g.nombre, g.area, g.numero_trabajadores, g.produccion_estimada_mes, g.direccion,
                             g.latitud, g.longitud, g.descripcion, g.id_departamento, g.id_municipio, g.id_corregimiento, 
-                            g.id_vereda, g.corregimiento_vereda,ug.usuarios_id,ug.puntuacion, ug.esfavorita, ug.espropietario,
-                            (select count(*) from reseñas as r where g.id_granja=r.id_granja_pk_fk) as cantidad_reseñas
+                            g.id_vereda, g.corregimiento_vereda,ug.usuarios_id, ug.esfavorita, ug.espropietario,
+                            (select count(*) from reseñas as r where g.id_granja=r.id_granja_pk_fk) as cantidad_reseñas,
+                            (select avg(r.calificacion) from reseñas as r where id_granja_pk_fk = g.id_granja) as puntuacion
                   FROM granjas as g, usuarios_granjas as ug
                   WHERE ug.usuarios_id=? and g.id_granja=ug.id_granja_pk_fk  and ug.esfavorita=1`, 
                   [ id_user]
