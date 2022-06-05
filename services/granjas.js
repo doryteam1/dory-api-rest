03856400f78b9f81550e7d6c100bb3c5e3ae1660
Renzo Funes Meza <rfunez@gmail.com>
2022-06-05 08:40:32 -0500

    correccion

--- services/granjas.js
+++ services/granjas.js
@@ -471,7 +471,7 @@ async function create(body,token){
                                     (SELECT Concat(u2.nombres,' ',u2.apellidos) FROM  usuarios as u2 left join usuarios_granjas as ug2 on (u2.id = ug2.usuarios_id  and ug2.espropietario=1)  
                                     WHERE   ug2.id_granja_pk_fk=g.id_granja) as propietario, 
                                     (select ug2.esfavorita from usuarios_granjas as ug2 where ug2.id_granja_pk_fk=g.id_granja and ug2.usuarios_id=?) as favorita,
-                                    (select avg(r.puntuacion) from reseñas as r where id_granja_pk_fk = g.id_granja) as puntuacion
+                                    (select avg(r.calificacion) from reseñas as r where id_granja_pk_fk = g.id_granja) as puntuacion
                   FROM  granjas as g left join fotos as f on (f.id_granja_fk = g.id_granja)
                                     left join usuarios_granjas as ug on (g.id_granja = ug.id_granja_pk_fk)      
                   WHERE   g.id_municipio=? 
@@ -482,10 +482,10 @@ async function create(body,token){
             const offset = helper.getOffset(page, config.listPerPage);
             rows = await db.query(
               `SELECT DISTINCT   g.id_granja, g.nombre,g.area, g.numero_trabajadores, g.produccion_estimada_mes, g.direccion,g.descripcion,g.latitud,g.longitud, g.corregimiento_vereda, f.id_foto,f.imagen,(select count(*) from reseñas r1,granjas g1 where r1.id_granja_pk_fk=g1.id_granja and r1.id_granja_pk_fk= g.id_granja) as count_resenas,
-                                (select avg(puntuacion) from usuarios_granjas ug5 where g.id_granja=ug5.id_granja_pk_fk ) as puntuacion,
                                 (SELECT Concat(u2.nombres,' ',u2.apellidos) FROM  usuarios as u2 left join usuarios_granjas as ug2 on (u2.id = ug2.usuarios_id  and ug2.espropietario=1)  
                                 WHERE   ug2.id_granja_pk_fk=g.id_granja) as propietario, 
-                                0 as favorita
+                                0 as favorita,
+                                (select avg(r.calificacion) from reseñas as r where id_granja_pk_fk = g.id_granja) as puntuacion
               FROM  granjas as g left join fotos as f on (f.id_granja_fk = g.id_granja)
                                 left join usuarios_granjas as ug on (g.id_granja = ug.id_granja_pk_fk)      
               WHERE   g.id_municipio=? 
