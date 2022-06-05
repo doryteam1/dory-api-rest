07af3951bb3885f7db13388b154cfb65f5d26ceb
Renzo Funes Meza <rfunez@gmail.com>
2022-06-05 09:47:57 -0500

    correccion de puntuacion en el endpoint granjas de usuario

--- services/granjas.js
+++ services/granjas.js
@@ -8,11 +8,12 @@ const {validarToken} = require ('../middelware/auth');
 async function getGranjaUsuario(page = 1,id_user){
       const offset = helper.getOffset(page, config.listPerPage);
       const rows = await db.query(
-        `SELECT g.*, u.cedula, u.nombres, u.apellidos, u.celular, ug.puntuacion 
+        `SELECT g.*, u.cedula, u.nombres, u.apellidos, u.celular,
+                (select avg(r.calificacion) from reseñas as r where id_granja_pk_fk = ?) as puntuacion 
         FROM granjas as g, usuarios as u, usuarios_granjas as ug
         WHERE u.id=? and ug.usuarios_id=? and ug.espropietario=1 and g.id_granja=ug.id_granja_pk_fk
         LIMIT ?,?`, 
-        [id_user, id_user, offset, config.listPerPage]
+        [id_user,id_user, id_user, offset, config.listPerPage]
       );
       const data = [];
       if(rows.length>0){
