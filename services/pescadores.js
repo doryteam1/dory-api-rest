a1dcb6c0ea456bd708e6535871920f68edfc5fb0
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-06-23 09:34:02 -0500

    cambios en endpoint getpiscicultoresMunicipio y getpescadoresmunicipio

--- services/pescadores.js
+++ services/pescadores.js
@@ -55,8 +55,8 @@ d.id_departamento=?
 async function getPescadoresAsociacion(page = 1,nit){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT distinctrow   tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.cedula,concat(u.nombres," ",u.apellidos) as nombre,
-                          u.celular,u.direccion,u.email,u.password,u.id_area_experticia,
+    `SELECT distinctrow   tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.id,u.cedula,concat(u.nombres," ",u.apellidos) as nombre,
+                          u.celular,u.direccion,u.email,u.id_area_experticia,
                           (select a.nombre from areas_experticias a  where a.id_area=u.id_area_experticia) as area_experticia,u.nombre_negocio,u.foto,u.fecha_registro,u.fecha_nacimiento,
                           (select d.nombre_departamento from departamentos d  where d.id_departamento=u.id_departamento) as departamento,
                           (select m.nombre from municipios as m  where m.id_municipio=u.id_municipio) as municipio,
--- services/piscicultores.js
+++ services/piscicultores.js
@@ -5,8 +5,8 @@ const config = require('../config');
 async function getPiscicultoresMunicipio(page = 1,idMunicipio){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
- `SELECT distinctrow   tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.cedula,concat(u.nombres," ",u.apellidos) as nombre,
-    u.celular,u.direccion,u.email,u.password,u.id_area_experticia,
+ `SELECT distinctrow   tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.id,u.cedula,concat(u.nombres," ",u.apellidos) as nombre,
+    u.celular,u.direccion,u.email,u.id_area_experticia,
     (select a.nombre from areas_experticias a  where a.id_area=u.id_area_experticia) as area_experticia,u.nombre_negocio,u.foto,u.fecha_registro,u.fecha_nacimiento,
     (select d.nombre_departamento from departamentos d  where d.id_departamento=u.id_departamento) as departamento,
     (select m.nombre from municipios as m  where m.id_municipio=u.id_municipio) as municipio,
