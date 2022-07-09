d03f25b7e4297225beb218cb1e80a952594a8243
Renzo Funes Meza <rfunez@gmail.com>
2022-07-09 14:29:01 -0500

    correccion id usuario piscicultores x municipio

--- services/pescadores.js
+++ services/pescadores.js
@@ -6,7 +6,7 @@ var createError = require('http-errors');
 async function getPescadoresMunicipio(page = 1,idMunicipio){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
- `SELECT distinctrow   tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.cedula,concat(u.nombres," ",u.apellidos) as nombre,
+ `SELECT distinctrow   u.id, tu.id_tipo_usuario,tu.nombre_tipo_usuario as tipo_usuario,u.cedula,concat(u.nombres," ",u.apellidos) as nombre,
     u.celular,u.direccion,u.email,u.id_area_experticia,
     (select a.nombre from areas_experticias a  where a.id_area=u.id_area_experticia) as area_experticia,u.nombre_negocio,u.foto,u.fecha_registro,u.fecha_nacimiento,
     (select d.nombre_departamento from departamentos d  where d.id_departamento=u.id_departamento) as departamento,
