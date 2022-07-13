b11e44cb63aaee9cfd4b171742aaf9e89c7e8ed8
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-07-13 15:47:32 -0500

    Buscar Endpoint buscar granja por cadena ok

--- routes/buscar-granja.js
+++ routes/buscar-granja.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 const buscarGranja = require('../services/buscar-granja');
 
-
 router.get('/:cadena', async function(req, res, next) {
   try {
-    res.json(await buscarGranja.getMultiple(req.query.page,req.params.cadena));
+    res.json(await buscarGranja.buscarGranjaCadena(req.query.page,req.params.cadena));
   } catch (err) {
     console.error(`Error al traer las granjas con esa cadena `, err.message);
     next(err);
   }
 });
-
 
 module.exports = router;
