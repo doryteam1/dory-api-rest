5f1c3c37923c1b6f3d1d032c4074492c450e9760
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-07-30 11:00:46 -0500

    Endpoint obtener todos los piscicultores realizado

--- routes/piscicultores.js
+++ routes/piscicultores.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const piscicultores = require('../services/piscicultores');
 
+router.get('/todos', async function(req, res, next) {
+  try {
+    res.json(await piscicultores.getPiscicultoresTodos(req.query.page));
+  } catch (err) {
+    console.error(`Error al traer los piscicultores del sistema `, err.message);
+    next(err);
+  }
+});
+
 router.get('/municipio/:idMunicipio', async function(req, res, next) {
   try {
     res.json(await piscicultores.getPiscicultoresMunicipio(req.query.page,req.params.idMunicipio));
