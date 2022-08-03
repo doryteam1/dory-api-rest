6af2e84726cab0ab72d5b41c5c50999ab129b5d2
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-08-02 22:47:12 -0500

    Adición de creadoCon en el endpoint login con Google

--- services/login.js
+++ services/login.js
@@ -57,50 +57,53 @@ async function createLogin(user){
 }//fin método
 
 async function loginWithGoogle(req){
-  if(!req.body.token){
-    throw createError(400,'Debe proporcionar un token.');
-  }
-  const token = req.body.token;
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: [CLIENT_ID_1, CLIENT_ID_2]  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  const payload = ticket.getPayload();
-  let email = payload.email;
-  const rows = await db.query('select * from usuarios as u left join tipos_usuarios as tu on u.id_tipo_usuario = tu.id_tipo_usuario where u.email = ?',[email]);
-  if(rows.length < 1){
-    throw createError(400,'El usuario no esta registrado.');
-  }
-  if(payload.email_verified){
-    if(!rows[0].estaVerificado){
-      const result = await db.query(
-        `UPDATE usuarios
-          SET estaVerificado=?
-          WHERE email=?`,
-          [
-            1,
-            email
-          ] 
-      );
-      if (result.affectedRows<1) {
-        throw createError(500,"Ocurrio un problema al autenticar el usuario. Intentelo nuevamente");
-      }
-    }
-  }else{
-    throw createError(403,"Este usuario tiene un correo que aun no ha sido verificado por google. Verifique primero su correo y luego intente la operación nuevamente.");
-  }
-  try{
-    const newToken = await helper.createToken(rows[0],ONE_YEAR_MILLISECONDS);  
-    return {
-      token: newToken,
-      authenticated:true
-    };
-  }catch(err){
-    console.log(err);
-    throw err;
-  }
+          if(!req.body.token){
+            throw createError(400,'Debe proporcionar un token.');
+          }
+          const token = req.body.token;
+          const ticket = await client.verifyIdToken({
+              idToken: token,
+              audience: [CLIENT_ID_1, CLIENT_ID_2]  // Specify the CLIENT_ID of the app that accesses the backend
+              // Or, if multiple clients access the backend:
+              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+          });
+          const payload = ticket.getPayload();
+          let email = payload.email;
+          const rows = await db.query(
+            'select * from usuarios as u left join tipos_usuarios as tu on u.id_tipo_usuario = tu.id_tipo_usuario where u.email = ?',[email]);
+          if(rows.length < 1){
+            throw createError(400,'El usuario no esta registrado.');
+          }
+          if(payload.email_verified){
+            if(!rows[0].estaVerificado){
+              const result = await db.query(
+                `UPDATE usuarios
+                  SET estaVerificado=?
+                  WHERE email=?`,
+                  [
+                    1,
+                    email
+                  ] 
+              );
+              if (result.affectedRows<1) {
+                throw createError(500,"Ocurrio un problema al autenticar el usuario. Intentelo nuevamente");
+              }
+            }
+          }else{
+            throw createError(403,"Este usuario tiene un correo que aun no ha sido verificado por google. Verifique primero su correo y luego intente la operación nuevamente.");
+          }
+          try{
+            let creadoCon =  'Google';   
+            const newToken = await helper.createToken(rows[0],ONE_YEAR_MILLISECONDS);  
+            return {
+              token: newToken,
+              authenticated:true,
+              creadoCon
+            };
+          }catch(err){
+            console.log(err);
+            throw err;
+          }
 }
 
 module.exports = {
