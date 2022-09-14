f615edade4610f627cebf086b7f9d7d8724ec311
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-09-14 11:39:33 -0500

    Se modificarón los endpoints de areas de experticia

--- services/areas_experticias.js
+++ services/areas_experticias.js
@@ -2,6 +2,7 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
+/*_____________________ getAreaExperticia______________________________________________*/
 async function getAreasExperticia(page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
@@ -14,55 +15,74 @@ async function getAreasExperticia(page = 1){
     data,
     meta
   }
-}
+}/*End getAreaExperticia*/
 
+
+/*_____________________ createAreaExperticia______________________________________________*/
 async function createAreaExperticia(area_experticia){
-    const result = await db.query(
-      `INSERT INTO areas_experticias(id_area,nombre,descripcion) VALUES (?,?,?)`, 
-      [
-        area_experticia.id_area,
-        area_experticia.nombre, 
-        area_experticia.descripcion
-      ]
-    );  
-    let message = 'Error creando Area de Experticia';  
-    if (result.affectedRows) {
-      message = 'Area de Experticia creada exitosamente';
-    }  
-    return {message};
-  }
+        if(area_experticia.id_area === undefined || 
+           area_experticia.nombre === undefined ||
+           area_experticia.descripcion === undefined ){
+                throw createError(400,"Debe enviar todos los datos requeridos para el registro del área de experticia");
+        }
+          try{
+                const result = await db.query(
+                  `INSERT INTO areas_experticias(id_area,nombre,descripcion) VALUES (?,?,?)`, 
+                  [
+                    area_experticia.id_area,
+                    area_experticia.nombre, 
+                    area_experticia.descripcion
+                  ]
+                );  
+                let message = 'Error creando Area de Experticia';  
+                if (result.affectedRows) {
+                  message = 'Area de Experticia creada exitosamente';
+                }else {
+                      throw createError(500,"Ocurrió un problema al registrar un aárea de experticia");
+                }
+            }catch(err){
+              throw err;
+            }      
+            return {message};
+  }/*End createAreaExperticia*/
 
+  /*_____________________ updateAreaExperticia______________________________________________*/
   async function updateAreaExperticia(id, area_experticia){
-    const result = await db.query(
-      `UPDATE areas_experticias
-       SET nombre=?,
-           descripcion=? 
-       WHERE id_area=?`,
-       [
-         area_experticia.nombre, 
-         area_experticia.descripcion,
-         id
-       ] 
-    );
+          if( 
+            area_experticia.nombre === undefined ||
+            area_experticia.descripcion === undefined ){
+                throw createError(400,"Debe enviar todos los datos requeridos para la actualización del área de experticia");
+          }
+            const result = await db.query(
+            `UPDATE areas_experticias
+            SET nombre=?,
+                descripcion=? 
+            WHERE id_area=?`,
+            [
+              area_experticia.nombre, 
+              area_experticia.descripcion,
+              id
+            ] 
+          );  
+          let message = 'Error actualizando Area de Experticia';  
+          if (result.affectedRows) {
+            message = 'Area de experticia actualizada exitosamente';
+          }  
+          return {message};
+  }/*End updateAreaExperticia*/
   
-    let message = 'Error actualizando Area de Experticia';  
-    if (result.affectedRows) {
-      message = 'Area de experticia actualizada exitosamente';
-    }  
-    return {message};
-  }
-  
+  /*______________________ removeAreaExperticia_______________________________*/
   async function removeAreaExperticia(id){
-    const result = await db.query(
-      `DELETE FROM areas_experticias WHERE id_area=?`, 
-      [id]
-    );  
-    let message = 'Error borrando Area de Experticia';  
-    if (result.affectedRows) {
-      message = 'Area de Experticia borrada exitosamente';
-    }  
-    return {message};
-  }
+        const result = await db.query(
+          `DELETE FROM areas_experticias WHERE id_area=?`, 
+          [id]
+        );  
+        let message = 'Error borrando Area de Experticia';  
+        if (result.affectedRows) {
+          message = 'Area de Experticia borrada exitosamente';
+        }  
+        return {message};
+  }/*End removeAreaExperticia*/
 
 module.exports = {
   getAreasExperticia,
