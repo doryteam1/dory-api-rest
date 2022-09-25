35bee6d2cfbea550779bbafb0ed416c2cc4886d3
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-09-25 10:52:09 -0500

    validación de admin en normatividades

--- routes/normatividades.js
+++ routes/normatividades.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const normatividades = require('../services/normatividades');
 
-
 router.get('/', async function(req, res, next) {
   try {
     res.json(await normatividades.getMultiple(req.query.page));
@@ -12,33 +11,33 @@ router.get('/', async function(req, res, next) {
   }
 });
 
-
 router.post('/', async function(req, res, next) {
     try {
-      res.json(await normatividades.create(req.body));
+          var token=req.headers.authorization;
+          res.json(await normatividades.create(req.body,token));
     } catch (err) {
-      console.error(`Error registrando normatividad`, err.message);
-      next(err);
+          console.error(`Error registrando normatividad`, err.message);
+          next(err);
     }
   });
 
-
 router.put('/:id', async function(req, res, next) {
     try {
-      res.json(await normatividades.update(req.params.id, req.body));
+          var token=req.headers.authorization;
+          res.json(await normatividades.update(req.params.id, req.body,token));
     } catch (err) {
-      console.error(`Error al actualizar normatividad`, err.message);
-      next(err);
+          console.error(`Error al actualizar normatividad`, err.message);
+          next(err);
     }
 });
 
-
 router.delete('/:id', async function(req, res, next) {
     try {
-      res.json(await normatividades.remove(req.params.id));
+          var token=req.headers.authorization;
+          res.json(await normatividades.remove(req.params.id,token));
     } catch (err) {
-      console.error(`Error al borrar normatividad`, err.message);
-      next(err);
+          console.error(`Error al borrar normatividad`, err.message);
+          next(err);
     }
   });
 
--- services/normatividades.js
+++ services/normatividades.js
@@ -1,89 +1,130 @@
 const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
-
+/*___________________________getMultiple___________________________________________*/
 async function getMultiple(page = 1){
-  const offset = helper.getOffset(page, config.listPerPage);
-  const rows = await db.query(
-    `SELECT normatividades.id as id_normatividad, concat(tipos_normatividades.nombre,' ',normatividades.nombre) as nombre,  normatividades.contenido AS contenido,normatividades.url_descarga AS url_descarga, normatividades.id_tipo_fk as id_tipo,tipos_normatividades.nombre AS tipo, normatividades.fecha as fecha
-    FROM normatividades,tipos_normatividades
-    WHERE normatividades.id_tipo_fk=tipos_normatividades.id_tipo LIMIT ?,?`, 
-    [offset, config.listPerPage]
-  );
-  const data = helper.emptyOrRows(rows);
-  const meta = {page};
+      const offset = helper.getOffset(page, config.listPerPage);
+      const rows = await db.query(
+        `SELECT normatividades.id as id_normatividad, concat(tipos_normatividades.nombre,' ',normatividades.nombre) as nombre,  normatividades.contenido AS contenido,normatividades.url_descarga AS url_descarga, normatividades.id_tipo_fk as id_tipo,tipos_normatividades.nombre AS tipo, normatividades.fecha as fecha
+        FROM normatividades,tipos_normatividades
+        WHERE normatividades.id_tipo_fk=tipos_normatividades.id_tipo LIMIT ?,?`, 
+        [offset, config.listPerPage]
+      );
+      const data = helper.emptyOrRows(rows);
+      const meta = {page};
+      return {
+        data,
+        meta
+      }
+}/*End getMultiple*/
 
-  return {
-    data,
-    meta
-  }
-}
+/*___________________________create___________________________________________*/
+async function create(normatividad,token){
+    try{
+            if(token && validarToken(token)){
+                    let payload=helper.parseJwt(token);
+                    let tipo_user= payload.rol; 
+                  if(tipo_user!='Administrador'){
+                          throw createError(401,"Usted no tiene autorización para registrar eventos");
+                  }
+                  if( normatividad.id===undefined ||                     
+                      normatividad.nombre===undefined ||  
+                      normatividad.contenido===undefined || 
+                      normatividad.url_descarga===undefined || 
+                      normatividad.id_tipo_fk===undefined || 
+                      normatividad.fecha===undefined                   
+                    )
+                    {
+                      throw createError(400,"Se requieren todos los parámetros de la normatividad");
+                    }
+                  const result = await db.query(
+                    `INSERT INTO normatividades(id,nombre, contenido,url_descarga,id_tipo_fk,fecha) VALUES (?,?,?,?,?,?)`, 
+                    [
+                    normatividad.id,
+                    normatividad.nombre, 
+                    normatividad.contenido,
+                    normatividad.url_descarga, 
+                    normatividad.id_tipo_fk,
+                    normatividad.fecha       
+                    ]
+                  );  
+                  let message = 'Error creando normatividad';  
+                  if (result.affectedRows) {
+                    message = 'La normatividad se registro exitosamente';
+                  }  
+                  return {message};
+            }else{ 
+                  throw createError(401,"Usted no tiene autorización"); 
+            }
+     }catch{
+             throw error;
+     }
+  }/*End create*/
 
-
-async function create(normatividad){
-    const result = await db.query(
-      `INSERT INTO normatividades(id,nombre, contenido,url_descarga,id_tipo_fk,fecha) VALUES (?,?,?,?,?,?)`, 
-      [
-       normatividad.id,
-       normatividad.nombre, 
-       normatividad.contenido,
-       normatividad.url_descarga, 
-       normatividad.id_tipo_fk,
-       normatividad.fecha       
-      ]
-    );
-  
-    let message = 'Error creando normatividad';
-  
-    if (result.affectedRows) {
-      message = 'La normatividad se registro exitosamente';
-    }
-  
-    return {message};
-  }
-
-  async function update(id, normatividad){
-    const result = await db.query(
-      `UPDATE normatividades
-       SET nombre=?,
-           contenido=?,
-           url_descarga=?, 
-           id_tipo_fk=?,
-           fecha=?
-       WHERE id=?`,
-       [
-        normatividad.nombre, 
-        normatividad.contenido,
-        normatividad.url_descarga, 
-        normatividad.id_tipo_fk,
-        normatividad.fecha,
-        id
-       ] 
-    );
-  
-    let message = 'Error actualizando normatividad';
-  
-    if (result.affectedRows) {
-      message = 'Registro de normatividad actualizado exitosamente';
-    }
-  
-    return {message};
-  }
-  
-  async function remove(id){
-    const result = await db.query(
-      `DELETE FROM normatividades WHERE id=?`, 
-      [id]
-    );
-  
-    let message = 'Error borrando normatividad';
-  
-    if (result.affectedRows) {
-      message = 'Registro de normatividad borrado exitosamente';
-    }
-  
-    return {message};
-  }
+  /*___________________________update___________________________________________*/
+  async function update(id, normatividad, token){
+        try{
+              if(token && validarToken(token)){
+                      let payload=helper.parseJwt(token);
+                      let tipo_user= payload.rol; 
+                    if(tipo_user!='Administrador'){
+                            throw createError(401,"Usted no tiene autorización para registrar eventos");
+                    }
+                  const result = await db.query(
+                    `UPDATE normatividades
+                    SET nombre=?,
+                        contenido=?,
+                        url_descarga=?, 
+                        id_tipo_fk=?,
+                        fecha=?
+                    WHERE id=?`,
+                    [
+                      normatividad.nombre, 
+                      normatividad.contenido,
+                      normatividad.url_descarga, 
+                      normatividad.id_tipo_fk,
+                      normatividad.fecha,
+                      id
+                    ] 
+                  );  
+                  let message = 'Error actualizando normatividad';  
+                  if (result.affectedRows) {
+                    message = 'Registro de normatividad actualizado exitosamente';
+                  }  
+                  return {message};
+              }else{ 
+                  throw createError(401,"Usted no tiene autorización"); 
+              }
+        }catch{
+                throw error;
+        }
+  }/*End update*/
+ 
+/*___________________________remove___________________________________________*/  
+  async function remove(id,token){
+          try{
+                if(token && validarToken(token)){
+                        let payload=helper.parseJwt(token);
+                        let tipo_user= payload.rol; 
+                      if(tipo_user!='Administrador'){
+                              throw createError(401,"Usted no tiene autorización para registrar eventos");
+                      }
+                      const result = await db.query(
+                        `DELETE FROM normatividades WHERE id=?`, 
+                        [id]
+                      );  
+                      let message = 'Error borrando normatividad';  
+                      if (result.affectedRows) {
+                        message = 'Registro de normatividad borrado exitosamente';
+                      }  
+                      return {message};
+                }else{ 
+                      throw createError(401,"Usted no tiene autorización"); 
+                }
+            }catch{
+                    throw error;
+            }     
+  }/*End remove*/
 
 module.exports = {
   getMultiple,
