279d3b619257fcca0e3506bd102f1a05cf2440cc
Renzo Funes Meza <rfunez@gmail.com>
2022-09-26 06:19:15 -0500

    de aciciono campo url_sisben url_imagen_cedula a get user by email

--- services/buscar-usuario-email.js
+++ services/buscar-usuario-email.js
@@ -24,7 +24,7 @@ async function getMultiple(page = 1, email){
           (select c.nombre from corregimientos as c  where c.id_corregimiento=u.id_corregimiento) as corregimiento,
           u.id_vereda,
           (select v.nombre from veredas as v  where v.id_vereda=u.id_vereda) as vereda,
-          u.latitud,u.longitud,u.nombre_corregimiento,u.nombre_vereda,u.estaVerificado,u.otra_area_experticia,u.otra_area_experticia_descripcion,u.sobre_mi, u.takeTour
+          u.latitud,u.longitud,u.nombre_corregimiento,u.nombre_vereda,u.estaVerificado,u.otra_area_experticia,u.otra_area_experticia_descripcion,u.sobre_mi, u.takeTour, u.url_imagen_cedula, u.url_sisben
           FROM usuarios as u left join tipos_usuarios as tu  on u.id_tipo_usuario=tu.id_tipo_usuario
           WHERE   u.email=?
           LIMIT ?,?`, 
