b66a61a16971af24d17e4f6f5a9a50735c994467
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-09-29 22:25:32 -0500

    validación y corrección de endpoints eventos

--- services/eventos.js
+++ services/eventos.js
@@ -7,7 +7,11 @@ const {validarToken} = require ('../middelware/auth');
 async function getMultiple(page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT * FROM eventos LIMIT ?,?`, 
+    `SELECT e.*,te.nombre as tipo, m.nombre as modalidad
+     FROM  eventos as e inner join tipos_eventos as te on e.id_tipo_evento_fk=te.id_evento
+                        inner join modalidades as m on e.id_modalidad_fk=m.id_modalidad
+     order by e.id_evento asc
+     LIMIT ?,?`, 
     [offset, config.listPerPage]
   );
   const data = helper.emptyOrRows(rows);
@@ -24,10 +28,9 @@ async function getEventosCapacitaciones(page = 1, cadena){
       let cad= '%'+cadena+'%';
       const rows = await db.query(
         `SELECT distinct e.id_evento, e.nombre as nombre,e.url, e.resumen, e.fecha, e.hora, e.dirigidoa, e.organizador, e.costo, e.imagen, te.nombre as tipo
-        FROM eventos as e inner join tipos_eventos as te  
-                    on ((te.nombre like '%Capacitaciones%') and (e.id_tipo_evento_fk=te.id_evento))  and                                            
-                        (e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?) 
-                        LIMIT ?,?`, 
+         FROM eventos as e inner join tipos_eventos as te  on ((te.nombre like '%Capacitaciones%') and (e.id_tipo_evento_fk=te.id_evento))                                              
+         WHERE e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?
+         LIMIT ?,?`, 
         [cad, cad, cad, cad, offset, config.listPerPage]
       );  
       const data = helper.emptyOrRows(rows);
@@ -44,10 +47,9 @@ async function getEventosCongresos(page = 1, cadena){
       let cad= '%'+cadena+'%';
       const rows = await db.query(
         `SELECT distinct e.id_evento, e.nombre as nombre,e.url, e.resumen, e.fecha, e.hora, e.dirigidoa, e.organizador, e.costo, e.imagen, te.nombre as tipo
-        FROM eventos as e inner join tipos_eventos as te  
-                    on ((te.nombre like '%Congresos%') and (e.id_tipo_evento_fk=te.id_evento))  and                                            
-                        (e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?) 
-                        LIMIT ?,?`, 
+        FROM eventos as e inner join tipos_eventos as te on ((te.nombre like '%Congresos%') and (e.id_tipo_evento_fk=te.id_evento))                                              
+        WHERE e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ? 
+        LIMIT ?,?`, 
         [cad, cad, cad, cad, offset, config.listPerPage]
       );  
       const data = helper.emptyOrRows(rows);
@@ -64,10 +66,9 @@ async function getEventosCursos(page = 1, cadena){
       let cad= '%'+cadena+'%';
       const rows = await db.query(
         `SELECT distinct e.id_evento, e.nombre as nombre,e.url, e.resumen, e.fecha, e.hora, e.dirigidoa, e.organizador, e.costo, e.imagen, te.nombre as tipo
-        FROM eventos as e inner join tipos_eventos as te  
-                    on ((te.nombre like '%Cursos%') and (e.id_tipo_evento_fk=te.id_evento))  and                                            
-                        (e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?) 
-                        LIMIT ?,?`, 
+        FROM eventos as e inner join tipos_eventos as te  on ((te.nombre like '%Cursos%') and (e.id_tipo_evento_fk=te.id_evento))                                              
+        WHERE e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?
+         LIMIT ?,?`, 
         [cad, cad, cad, cad, offset, config.listPerPage]
       );
       const data = helper.emptyOrRows(rows);
@@ -85,10 +86,9 @@ async function getEventosDiplomados(page = 1, cadena){
       let cad= '%'+cadena+'%';
       const rows = await db.query(
         `SELECT distinct e.id_evento, e.nombre as nombre,e.url, e.resumen, e.fecha, e.hora, e.dirigidoa, e.organizador, e.costo, e.imagen, te.nombre as tipo
-        FROM eventos as e inner join tipos_eventos as te  
-                    on ((te.nombre like '%Diplomados%') and (e.id_tipo_evento_fk=te.id_evento))  and                                            
-                        (e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?) 
-                        LIMIT ?,?`, 
+        FROM eventos as e inner join tipos_eventos as te  on ((te.nombre like '%Diplomados%') and (e.id_tipo_evento_fk=te.id_evento))  
+        WHERE    e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ? 
+        LIMIT ?,?`, 
         [cad, cad, cad, cad, offset, config.listPerPage]
       );
       const data = helper.emptyOrRows(rows);
@@ -105,10 +105,9 @@ async function getEventosSeminarios(page = 1, cadena){
       let cad= '%'+cadena+'%';
       const rows = await db.query(
         `SELECT distinct e.id_evento, e.nombre as nombre,e.url, e.resumen, e.fecha, e.hora, e.dirigidoa, e.organizador, e.costo, e.imagen, te.nombre as tipo
-        FROM eventos as e inner join tipos_eventos as te  
-                    on ((te.nombre like '%Seminarios%') and (e.id_tipo_evento_fk=te.id_evento))  and                                            
-                        (e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?) 
-                        LIMIT ?,?`, 
+        FROM eventos as e inner join tipos_eventos as te on ((te.nombre like '%Seminarios%') and (e.id_tipo_evento_fk=te.id_evento))                                         
+        WHERE e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?
+        LIMIT ?,?`, 
         [cad, cad, cad, cad, offset, config.listPerPage]
       );
       const data = helper.emptyOrRows(rows);
@@ -121,23 +120,21 @@ async function getEventosSeminarios(page = 1, cadena){
 
 /*___________________________________________ getEventosTalleres______________________________________ */
 async function getEventosTalleres(page = 1, cadena){
-  const offset = helper.getOffset(page, config.listPerPage);
-  let cad= '%'+cadena+'%';
-  const rows = await db.query(
-    `SELECT distinct e.id_evento, e.nombre as nombre,e.url, e.resumen, e.fecha, e.hora, e.dirigidoa, e.organizador, e.costo, e.imagen, te.nombre as tipo
-    FROM eventos as e inner join tipos_eventos as te  
-                 on ((te.nombre like '%Talleres%') and (e.id_tipo_evento_fk=te.id_evento))  and                                            
-                     (e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?) 
-                     LIMIT ?,?`, 
-    [cad, cad, cad, cad, offset, config.listPerPage]
-  );
-  const data = helper.emptyOrRows(rows);
-  const meta = {page};
-
-  return {
-    data,
-    meta
-  }
+          const offset = helper.getOffset(page, config.listPerPage);
+          let cad= '%'+cadena+'%';
+          const rows = await db.query(
+            `SELECT distinct e.id_evento, e.nombre as nombre,e.url, e.resumen, e.fecha, e.hora, e.dirigidoa, e.organizador, e.costo, e.imagen, te.nombre as tipo
+            FROM eventos as e inner join tipos_eventos as te on ((te.nombre like '%Talleres%') and (e.id_tipo_evento_fk=te.id_evento))                                             
+            WHERE e.nombre like ? or e.resumen like ? or e.organizador like ? or e.dirigidoa like ?
+            LIMIT ?,?`, 
+            [cad, cad, cad, cad, offset, config.listPerPage]
+          );
+          const data = helper.emptyOrRows(rows);
+          const meta = {page};
+          return {
+            data,
+            meta
+          }
 }/*End getEventosTalleres */
 
 /*__________________________________ getEventosTipos_______________________________ */
@@ -223,6 +220,23 @@ async function create(evento,token){
                       if(tipo_user!='Administrador'){
                               throw createError(401,"Usted no tiene autorización para actualizar eventos");
                       }
+                      if(
+                        evento.nombre===undefined || 
+                        evento.resumen===undefined || 
+                        evento.fecha===undefined ||
+                        evento.hora===undefined || 
+                        evento.imagen===undefined ||
+                        evento.url===undefined ||
+                        evento.dirigidoa===undefined || 
+                        evento.organizador===undefined || 
+                        evento.costo===undefined ||
+                        evento.id_modalidad===undefined || 
+                        evento.id_tipo_evento===undefined 
+                      )
+                      {
+                        throw createError(400,"Se requieren todos los parámetros del evento");
+                      }
+
                                   const result = await db.query(
                                     `UPDATE eventos 
                                     SET nombre=?,
