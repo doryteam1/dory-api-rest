51b817ec8cca0b7614a9b1f1a87ea1da1e56a7a6
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-09-30 08:33:37 -0500

    crud de nosotros incompleto

---
+++ services/nosotros.js
@@ -0,0 +1,83 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+var createError = require('http-errors');
+
+/*_____________________ getNosotros______________________________________________*/
+async function getNosotros(page = 1){
+ 
+}/*End getAreaExperticia*/
+
+
+/*_____________________ createAreaExperticia______________________________________________*/
+async function registrarNosotros(){
+        if(area_experticia.id_area === undefined || 
+           area_experticia.nombre === undefined ||
+           area_experticia.descripcion === undefined ){
+                throw createError(400,"Debe enviar todos los datos requeridos para el registro del área de experticia");
+        }
+          try{
+                const result = await db.query(
+                  `INSERT INTO areas_experticias(id_area,nombre,descripcion) VALUES (?,?,?)`, 
+                  [
+                    area_experticia.id_area,
+                    area_experticia.nombre, 
+                    area_experticia.descripcion
+                  ]
+                );  
+                let message = 'Error creando Area de Experticia';  
+                if (result.affectedRows) {
+                  message = 'Area de Experticia creada exitosamente';
+                }else {
+                      throw createError(500,"Ocurrió un problema al registrar un aárea de experticia");
+                }
+            }catch(err){
+              throw err;
+            }      
+            return {message};
+  }/*End createAreaExperticia*/
+
+  /*_____________________ updateAreaExperticia______________________________________________*/
+  async function actualizarNosotros(id, area_experticia){
+          if( 
+            area_experticia.nombre === undefined ||
+            area_experticia.descripcion === undefined ){
+                throw createError(400,"Debe enviar todos los datos requeridos para la actualización del área de experticia");
+          }
+            const result = await db.query(
+            `UPDATE areas_experticias
+            SET nombre=?,
+                descripcion=? 
+            WHERE id_area=?`,
+            [
+              area_experticia.nombre, 
+              area_experticia.descripcion,
+              id
+            ] 
+          );  
+          let message = 'Error actualizando Area de Experticia';  
+          if (result.affectedRows) {
+            message = 'Area de experticia actualizada exitosamente';
+          }  
+          return {message};
+  }/*End updateAreaExperticia*/
+  
+  /*______________________ removeAreaExperticia_______________________________*/
+  async function eliminarNosotros(id){
+        const result = await db.query(
+          `DELETE FROM areas_experticias WHERE id_area=?`, 
+          [id]
+        );  
+        let message = 'Error borrando Area de Experticia';  
+        if (result.affectedRows) {
+          message = 'Area de Experticia borrada exitosamente';
+        }  
+        return {message};
+  }/*End removeAreaExperticia*/
+
+module.exports = {
+  getNosotros,
+  registrarNosotros,
+  actualizarNosotros,
+  eliminarNosotros
+}
