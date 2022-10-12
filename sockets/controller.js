c4bf9cdf811e0b48f350626603d52b80c89865e3
Renzo Funes Meza <rfunez@gmail.com>
2022-10-12 12:48:35 -0500

    sockets new message

--- sockets/controller.js
+++ sockets/controller.js
@@ -31,15 +31,16 @@ const socketController = async( socket = new Socket(), io ) => {
             io.emit('usuarios-activos', chatMensajes.usuariosArr );
         })
 
-         socket.on('enviar-mensaje', ({ uid, mensaje }) => {
-            if ( uid ) {
-                // Mensaje privado
-                socket.to( uid ).emit( 'mensaje-privado', { de: usuario.nombre, mensaje });
-            } 
-            /* else {
-                chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje );
-                io.emit('recibir-mensajes', chatMensajes.ultimos10 );
-            } */
+        socket.on('new-message', ({ uid, mensaje }) => {
+            console.log("message recived!")
+        if ( uid ) {
+            // Mensaje privado
+            socket.to( uid ).emit( 'new-message', { usuario, mensaje });
+        } 
+        /* else {
+            chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje );
+            io.emit('recibir-mensajes', chatMensajes.ultimos10 );
+        } */
         })
     
     }
