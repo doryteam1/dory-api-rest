669428b5cc90b4cee06e986660028c82742b37ca
Renzo Funes Meza <rfunez@gmail.com>
2022-10-20 21:49:31 -0500

    test

--- sockets/controller.js
+++ sockets/controller.js
@@ -53,7 +53,7 @@ const socketController = async (socket = new Socket(), io) => {
                     let result = await ChatDbService.createMessage(message, token)
                     console.log("Resultado guardado de mensaje->> ",result)
                     console.log("Enviar mensaje a usuario.sub", usuario.sub)
-                    //socket.to(uid).emit('new-message', { de: usuario.sub, mensaje, metadata:result });
+                    socket.to(uid).emit('new-message', { de: usuario.sub, mensaje, metadata:result });
                     socket.to(usuario.sub).emit('new-message', { de: usuario.sub, mensaje,metadata:result });
                 } catch (err) {
                     console.log(err)
