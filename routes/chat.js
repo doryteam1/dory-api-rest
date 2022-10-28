dabb01cdbaba1ad23865868762e222e99ce502f8
Renzo Funes Meza <rfunez@gmail.com>
2022-10-28 15:52:57 -0500

    ajuste nombre endpoint

--- routes/chat.js
+++ routes/chat.js
@@ -22,7 +22,7 @@ router.get('/ultimos/', async function(req, res, next) {
   }
 });
 
-router.get('/unreaded/', async function(req, res, next) {
+router.get('/unreads/', async function(req, res, next) {
   try {
         var token=req.headers.authorization;
         res.json(await chat.getUnreaded(token));
