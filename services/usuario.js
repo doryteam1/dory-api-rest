7d017967435b329b9e4ddec613bfc8c48b341d63
Luis Alberto Pontón Mercado <luis0130@gmail.com>
2022-11-08 09:18:15 -0500

    borrado de fotos de vehiculos y vehiculos al eliminar usuario3

--- services/usuario.js
+++ services/usuario.js
@@ -311,7 +311,7 @@ async function update(idUser, usuario, token){
                         [idUser]
                       );
                       for(let i=0;i<idVehiculos.length;i++){
-                        let veh=idVehiculos[i];
+                        let veh=idVehiculos[i]; console.log("Vehículo"+" ",veh);
                         await conection.execute(
                           `DELETE FROM fotosVehiculos WHERE id_vehiculo_fk=?`, 
                           [veh]
